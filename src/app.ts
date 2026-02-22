cc41d3a6d4bad08af6968c316bee2a50a3871bcb
MHMIZAN09 <mhmizan550@gmail.com>
2026-02-22 16:50:08 +0600

    added the better-auth authentication

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 import cors from 'cors';
+import { auth } from './lib/auth';
+import { toNodeHandler } from 'better-auth/node';
 
 const app = express();
 
@@ -9,5 +11,8 @@ app.use(express.json());
 app.get('/', (req, res) => {
   res.send('Welcome to the SkillBridge Backend API!');
 });
+
+// better auth
+app.all('/api/auth/*splat', toNodeHandler(auth));
 
 export default app;
--- src/config/index.ts
+++ src/config/index.ts
@@ -6,6 +6,9 @@ dotenv.config({ path: path.join(process.cwd(), '.env') });
 const config = {
   databaseUrl: process.env.DATABASE_URL,
   port: process.env.PORT,
+  betterAuthSecret: process.env.BETTER_AUTH_SECRET,
+  betterAuthUrl: process.env.BETTER_AUTH_URL,
+  appUrl: process.env.APP_URL,
 };
 
 export default config;
