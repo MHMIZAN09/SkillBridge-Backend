a65ef2b57ed65b23a79d15cdd646aa3e48fa4db7
MHMIZAN09 <mhmizan550@gmail.com>
2026-02-22 21:12:23 +0600

    add search and pagination

--- src/modules/tutor/tutor.controller.ts
+++ src/modules/tutor/tutor.controller.ts
@@ -1,10 +1,26 @@
 import { Request, Response } from 'express';
 import { TutorService } from './tutor.service';
+import paginationSortingHelper from '../../utills/paginationHelper';
 
 const getAllTutors = async (req: Request, res: Response) => {
   // Logic to retrieve all tutors from the database
   try {
-    const tutors = await TutorService.getAllTutors();
+    const filters = {
+      search: req.query.search ? (req.query.search as string) : null,
+      hourlyRate: req.query.hourlyRate ? Number(req.query.hourlyRate) : null,
+      categoryId: req.query.categoryId
+        ? (req.query.categoryId as string)
+        : null,
+      avgRating: req.query.avgRating ? Number(req.query.avgRating) : null,
+      totalReviews: req.query.totalReviews
+        ? Number(req.query.totalReviews)
+        : null,
+    };
+    const paginationOptions = paginationSortingHelper(req.query);
+    const tutors = await TutorService.getAllTutors({
+      ...filters,
+      ...paginationOptions,
+    });
     res.status(200).json({
       success: true,
       message: 'Tutors retrieved successfully',
--- src/modules/tutor/tutor.service.ts
+++ src/modules/tutor/tutor.service.ts
@@ -1,9 +1,112 @@
 import { prisma } from '../../lib/prisma';
 
-const getAllTutors = async () => {
+type FilterItems = {
+  search: string | null;
+  hourlyRate: number | null;
+  categoryId: string | null;
+  avgRating: number | null;
+  totalReviews: number | null;
+  page: number;
+  limit: number;
+  skip: number;
+  sortBy: string;
+  sortOrder: string;
+};
+
+const getAllTutors = async ({
+  search,
+  hourlyRate,
+  categoryId,
+  avgRating,
+  totalReviews,
+  page,
+  limit,
+  skip,
+  sortBy,
+  sortOrder,
+}: FilterItems) => {
   // Logic to fetch all tutors from the database
-  const tutors = await prisma.tutorProfile.findMany();
-  return tutors;
+
+  const andConditions: any[] = [];
+
+  if (search) {
+    andConditions.push({
+      OR: [
+        {
+          user: {
+            name: {
+              contains: search,
+              mode: 'insensitive',
+            },
+          },
+        },
+        {
+          bio: {
+            contains: search,
+            mode: 'insensitive',
+          },
+        },
+      ],
+    });
+  }
+  if (hourlyRate) {
+    andConditions.push({
+      hourlyRate: {
+        lte: hourlyRate,
+      },
+    });
+  }
+
+  if (categoryId) {
+    andConditions.push({
+      categoryId: categoryId,
+    });
+  }
+
+  if (avgRating) {
+    andConditions.push({
+      avgRating: {
+        gte: avgRating,
+      },
+    });
+  }
+
+  if (totalReviews) {
+    andConditions.push({
+      totalReviews: {
+        gte: totalReviews,
+      },
+    });
+  }
+
+  const tutors = await prisma.tutorProfile.findMany({
+    take: limit,
+    skip: skip,
+    orderBy: {
+      [sortBy]: sortOrder,
+    },
+    where: {
+      AND: andConditions,
+    },
+    include: {
+      user: true,
+      category: true,
+    },
+  });
+  const total = await prisma.tutorProfile.count({
+    where: {
+      AND: andConditions,
+    },
+  });
+  return {
+    tutors,
+    pagination: {
+      total,
+      page,
+      limit,
+      totalPages: Math.ceil(total / limit),
+    },
+  };
 };
 
 export const TutorService = {
